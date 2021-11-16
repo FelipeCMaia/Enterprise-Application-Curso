99fb9414f176463434c55936f35f65a82b8330ae
Felipe <felipe.cmaia@hotmail.com>
2021-11-16 07:55:51 -0300

    Ajustes iniciais da api de carrinho

---
+++ NerdStoreEnterprise/src/services/NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddScoped<IAspNetUser, AspNetUser>();
+            //services.AddScoped<CarrinhoContext>();
+        }
+    }
+}
